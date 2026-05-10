4e6570981908a584b9113845f6a0c556a4b39069
Elias Mateus <eliasmateusmgoncalves@gmail.com>
2026-05-10 19:35:03 -0300

    feat(backend): adiciona EmployeeCreateSerializer

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -19,3 +19,32 @@ class EmployeeSerializer(serializers.ModelSerializer):
 
     def get_user_name(self, obj):
         return obj.user.get_full_name() or obj.user.username
+
+
+class EmployeeCreateSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(write_only=True)
+    email = serializers.EmailField(write_only=True)
+    password = serializers.CharField(write_only=True, min_length=6)
+    school_id = serializers.IntegerField(write_only=True)
+    is_admin = serializers.BooleanField(write_only=True, default=False)
+
+    class Meta:
+        model = Employee
+        fields = ['username', 'email', 'password', 'school_id', 'role', 'is_admin']
+
+    def create(self, validated_data):
+        is_admin = validated_data.pop('is_admin', False)
+        school_id = validated_data.pop('school_id')
+        school = None
+        if school_id:
+            from core.models import School
+            school = School.objects.get(id=school_id)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password']
+        )
+        if is_admin:
+            user.is_staff = True
+            user.save()
+        return Employee.objects.create(user=user, school=school, **validated_data)
