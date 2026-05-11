90f00adf9959b2cd56127c8ea548fdbd469c981c
Elias Mateus <eliasmateusmgoncalves@gmail.com>
2026-05-10 21:09:59 -0300

    feat(frontend): adiciona interfaces School e DemandCategory

---
+++ frontend/types/core.ts
@@ -0,0 +1,13 @@
+export interface School {
+  id: number
+  name: string
+  city: string
+  inep_code: string
+  created_at: string
+}
+
+export interface DemandCategory {
+  id: number
+  name: string
+  description: string
+}
